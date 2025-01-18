3da3d443628082f33ed62125a2c8555d1ca0edff
P H <git@cubt85iz>
2025-01-18 00:02:47 -0500

    Fixes bugs in the audible-tool.sh script and refines output.

--- audible-tool.sh
+++ audible-tool.sh
@@ -2,22 +2,35 @@
 
 set -euo pipefail
 
-CONSUME_DIR=$1
-if [ -z "$CONSUME_DIR" ]; then
+# Parse input provided for consume directory.
+if [ -n "$1" ]; then
+  CONSUME_DIR=$(readlink -f "$1")
+else
   echo "ERROR: Directory for protected audiobooks not provided."
   exit 1
 fi
 
-STAGING_DIR=$2
-if [ -z "$STAGING_DIR" ]; then
+# Parse input provided for staging directory
+if [ -n "$2" ]; then
+  STAGING_DIR=$(readlink -f "$2")
+else
   echo "ERROR: Directory for cleaned audiobooks not provided."
   exit 1
 fi
 
-for FILE in "${CONSUME_DIR}/*.aax"; do
-  FILENAME=${FILE}
-  if ! grep "${FILE}" "${CONSUME_DIR}/.processed"; then
-    ffmpeg -activation_bytes $ACTIVATION_BYTES -i "$FILE" -c copy "${STAGING_DIR}/${FILENAME%.*}.m4b" && \
-    echo $FILE > "${CONSUME_DIR}/.processed"
+# Iterate over *.aax files in consume directory and convert to *.m4b if not previously processed.
+for FILE in "${CONSUME_DIR}"/*.aax; do
+  FILENAME=${FILE##*/}
+  if [ ! -f "${CONSUME_DIR}/.processed" ]; then
+    touch "${CONSUME_DIR}/.processed"
+  fi
+
+  if ! grep "${FILENAME}" "${CONSUME_DIR}/.processed" &> /dev/null; then
+    echo "Converting ${FILE}"
+    echo "-> ${STAGING_DIR}/${FILENAME%.*}.m4b"
+    ffmpeg -loglevel error -activation_bytes "$ACTIVATION_BYTES" -i "$FILE" -c copy "${STAGING_DIR}/${FILENAME%.*}.m4b" && \
+    echo "$FILENAME" >> "${CONSUME_DIR}/.processed"
+  else
+    echo "Skipping processed file ${FILE}."
   fi
 done
